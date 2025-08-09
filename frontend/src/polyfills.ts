d46bf6523d4fc15d649775f99970649b64cd5ae8
sva15 <85981504+sva15@users.noreply.github.com>
2025-08-09 11:05:27 +0530

    feat(ui): new changes

---
+++ frontend/src/polyfills.ts
@@ -0,0 +1,40 @@
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+/** Required for Angular to run properly in most browsers */
+import 'zone.js/dist/zone';  // Included with Angular CLI
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE9-11 & legacy Edge support */
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es6/weak-map';
+// import 'core-js/es6/set';
+
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run `npm install --save classlist.js`
+
+/** Evergreen browsers (Chrome, Firefox, Edge) support */
+import 'core-js/es/reflect';
+
+/** ALL Firefox browsers require the following to support `@angular/animation` */
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`
+
+/***************************************************************************************************
+ * Zone.js is required by default for Angular itself
+ */
+import 'zone.js/dist/zone';  // Included with Angular CLI
